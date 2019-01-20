edd7b9069d3e54d892bbc5c75f8e96c7324f9f84
siddh991 <siddh991@github.com>
2019-01-20 06:26:48 -0800

    added frontend

---
+++ deep-learning-opencv/frontend/js/dashboard.js
@@ -0,0 +1,10 @@
+let dropArea = document.getElementById('drop-area')
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+  })
+
+  function preventDefaults (e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }
