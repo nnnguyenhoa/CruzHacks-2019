40d67c508c6edd1a2a35ed1d365ee6bc1306565b
= <flashfirebharat@gmail.com>
2019-01-20 02:36:39 -0800

    done

--- deep-learning-opencv/test.js
+++ deep-learning-opencv/test.js
@@ -2,14 +2,17 @@
 var child = require('child_process').spawn('python', ['deep_learning_with_opencv.py' , '--image' , 'images/eagle.png', '--prototxt', 'bvlc_googlenet.prototxt', '--model', 'bvlc_googlenet.caffemodel', '--labels', 'synset_words.txt'])
 // runs when script closes
 child.on("close", (code, signal) => {
-    console.log("return code: " + code);
+    // console.log("return code: " + code);
 
-    // if code == 1:
-    //     console.log(organic)
-    // elif code == 2:
-    //     console.log(recycling)
-    // else:
-    //     console.log(garbage)
+    if (code == 1){
+        console.log("organic")
+    }
+    else if (code == 2){
+        console.log("recycling")
+    } 
+    else{
+        console.log("garbage")
+    }
 })
 
 // runs if script throws an error
